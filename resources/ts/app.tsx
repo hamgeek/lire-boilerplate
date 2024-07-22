4050fd78b6bf8f760301c13f8d83e5a1293cefaa
hamgeek <ham.intl.id@gmail.com>
2024-07-22 10:28:20 +0700

    init

---
+++ resources/ts/app.tsx
@@ -0,0 +1,16 @@
+import '../css/app.css';
+
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+
+createInertiaApp({
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.tsx`,
+            import.meta.glob('./pages/**/*.tsx'),
+        ),
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/ts/app.jsx'],
+            refresh: true,
+        }),
+        react(),
+    ],
+});
