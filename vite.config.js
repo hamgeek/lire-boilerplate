7392c0afcb396fa6229dafb63a88b0103f289b71
hamgeek <ham.intl.id@gmail.com>
2024-09-01 09:37:11 +0700

    feat: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/ts/app.jsx'],
+            input: ['resources/css/app.css', 'resources/ts/app.tsx'],
             refresh: true,
         }),
         react(),
     ],
+    resolve: {
+        alias: [
+          { find: '@', replacement: path.resolve(__dirname, 'resources/ts') }
+        ]
+      }
 });
